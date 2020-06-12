848bbf23352e03eb2a3e7095fa838e55141de318
Oleksandr Butrym <butrym@karazin.ua>
2020-06-12 03:34:39 +0300

    Point, distance

---
+++ Sprint01/Task05.java
@@ -0,0 +1,25 @@
+class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int[] getXYPair() {
+        return new int[]{x, y};
+    }
+
+    public double distance(int x, int y) {
+        return Math.hypot(this.x - x, this.y - y);
+    }
+
+    public double distance(Point point) {
+        return distance(point.x, point.y);
+    }
+    
+    public double distance() {
+        return distance(0, 0);
+    }
+}
