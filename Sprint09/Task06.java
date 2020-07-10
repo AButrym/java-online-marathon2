fdb2d555e465f04b6922dc8f4cbaadbdd9c0b811
Oleksandr Butrym <butrym@karazin.ua>
2020-07-10 12:24:52 +0300

    phoneNumbers
    The test is more adequate now
    Changed the explicit TreeSet (with implicit sorting) into explicit stream().sorted()

--- Sprint09/Task06.java
+++ Sprint09/Task06.java
@@ -4,7 +4,8 @@ import java.util.stream.Stream;
 import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.*;
 
-public class MyUtils {
+class MyUtils {
+
     public Map<String, Stream<String>> phoneNumbers(List<Stream<String>> list) {
         return list.stream().filter(Objects::nonNull)
                 .flatMap(x -> x).filter(Objects::nonNull)
@@ -16,18 +17,23 @@ public class MyUtils {
                                 : "err",
                         mapping(s -> s.length() == 10 ? s.substring(3) : s,
                                 collectingAndThen(
-                                        toCollection(TreeSet::new), // sorting
-                                        Collection::stream)
+                                        toSet(),
+                                        set -> set.stream().sorted())
                         )
                 ));
     }
 
     public static void main(String[] args) {
         // smoke test
-        var in = List.of(
-                Stream.of("093 987 65 43", "(050)1234567", "12-345"),
-                Stream.of("067-21-436-57", "050-2345678", "0939182736", "224-19-28"),
-                Stream.of("(093)-11-22-334", "044 435-62-18", "721-73-45"));
+        var in = new ArrayList<Stream<String>>() {{
+            add(Stream.of("093 987 65 43", "(050)1234567", "12-345"));
+            add(Stream.of("067-21-436-57", "050-2345678", "0939182736", "224-19-28"));
+            add(Stream.of("(093)-11-22-334", "044 435-62-18", "721-73-45", null));
+            add(Stream.of());
+            add(Stream.of("", " "));
+            add(Arrays.stream(new String[]{null}));
+            add(null);
+        }};
         var expected = Map.of(
                 "050", Stream.of("1234567", "2345678"),
                 "067", Stream.of("2143657"),
@@ -37,18 +43,19 @@ public class MyUtils {
                 "err", Stream.of("12345")
         );
         var out = new MyUtils().phoneNumbers(in);
-//        out.forEach((k,v) -> System.out.printf("%s : %s%n", k, v.collect(Collectors.joining(", ", "[", "]"))));
-//        System.out.println("<<GOT **************** EXPECTED>>");
-//        expected.forEach((k,v) -> System.out.printf("%s : %s%n", k, v.collect(Collectors.joining(", ", "[", "]"))));
-        /* Streams are for single use, so either print or test */
-        boolean res = out.entrySet().stream()
-                .allMatch(mapEntry ->
-                        expected.containsKey(mapEntry.getKey()) &&
-                                Arrays.equals(
-                                        expected.get(mapEntry.getKey()).toArray(),
-                                        mapEntry.getValue().toArray()
-                                )
-                );
-        System.out.println(res ? "OK" : "FAIL");
+
+        var outRead = out.entrySet().stream().collect(toMap(
+                Map.Entry::getKey, me -> me.getValue().collect(toList())));
+        var expRead = expected.entrySet().stream().collect(toMap(
+                Map.Entry::getKey, me -> me.getValue().collect(toList())));
+
+        boolean isOk = outRead.keySet().equals(expRead.keySet())
+                && Objects.deepEquals(outRead, expRead);
+        System.out.println(isOk ? "OK" : "FAIL");
+        if (!isOk) {
+            outRead.forEach((k, v) -> System.out.printf("%s : %s%n", k, v));
+            System.out.println("^^^ GOT **************** EXPECTED vvv");
+            expRead.forEach((k, v) -> System.out.printf("%s : %s%n", k, v));
+        }
     }
 }
